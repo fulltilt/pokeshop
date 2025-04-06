97e85d1a790b29328a9c47c00da19e04cf6e2fa8
David Doria <dave.doria@gmail.com>
2025-04-06 15:21:48 -0700

    attempt at using VERCEL_URL

--- app/(ui)/items/page.tsx
+++ app/(ui)/items/page.tsx
@@ -12,7 +12,7 @@ import { ItemSchema } from "@/components/AddToCartButton";
 
 export default async function ItemsPage() {
   // const items = await prismaClient.item.findMany();
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/items`);
+  const response = await fetch(`${process.env.VERCEL_URL}/api/items`);
   if (!response.ok) {
     throw new Error("Failed to fetch items");
   }
--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ import { ItemSchema } from "@/components/AddToCartButton";
 
 export default async function Home() {
   // const items = await prismaClient.item.findMany({ take: 3 });
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/items`);
+  const response = await fetch(`${process.env.VERCEL_URL}/api/items`);
   if (!response.ok) {
     throw new Error("Failed to fetch items");
   }
